95188db4d7e513d54a7953a1ae04719abaffc75b
zhongaishi92 <54693220+zhongaishi92@users.noreply.github.com>
2021-04-11 00:36:23 -0400

    Add files via upload

--- static/js/default2.js
+++ static/js/default2.js
@@ -1,42 +1,13 @@
 $(document).ready(function() {
-    // Use a "/test" namespace.
-    // An application can open a connection on multiple namespaces, and
-    // Socket.IO will multiplex all those connections on a single
-    // physical channel. If you don't care about multiple channels, you
-    // can set the namespace to an empty string.
-    namespace = '/test';
 
-    // Connect to the Socket.IO server.
-    // The connection URL has the following format, relative to the current page:
-    //     http[s]://<domain>:<port>[/<namespace>]
+    namespace = '/test';
     var socket = io(namespace);
 
-    // Event handler for new connections.
-    // The callback function is invoked when a connection with the
-    // server is established.
-    socket.on('connect', function() {
-        socket.emit('my_event', {data: 'I\'m connected!'});
-    });
-
-    // Event handler for server sent data.
-    // The callback function is invoked whenever the server emits data
-    // to the client. The data is then displayed in the "Received"
-    // section of the page.
-    socket.on('my_response', function(msg, cb) {
-        $('#log').append('<br>' + $('<div/>').text('Received #' + msg.count + ': ' + msg.data).html());
-
-        if (cb)
-            cb();
-    });
-
-    // Handler for the "pong" message. When the pong is received, the
-    // time from the ping is stored, and the average of the last 30
-    // samples is average and displayed.
     socket.on('blog_done', function(record) {
         var history = document.getElementById('chat');
         response = "<div class='history'><p>Uploaded by " + record.user + " by date: " + record.date + "</p><hr>";
-        if(record.image){
-            response += "<img src='static/images/" + record.image + "'>";
+        if(record.filename){
+            response += "<embed src='static/images/" + record.filename + "' type='"+ record.filetype + "' width='90%'>";
         }
         response += "<p>Description: " + record.comment + "</p><br/></div>"
 
@@ -44,17 +15,17 @@ $(document).ready(function() {
         history.innerHTML += response;
     });
 
-    // Handlers for the different forms in the page.
-    // These accept data from the user and send it to the server in a
-    // variety of ways
     $('button').click(function(){
         var comment = $('#comment').val();
         var file = document.getElementById('form-file').files[0];
         if(file){
+            console.log("type and name")
+            console.log(file.type)
+            console.log(file.name)
             var reader = new FileReader();
             reader.readAsDataURL(file);
             reader.addEventListener("load", function(){
-                socket.emit('send-message', {'comment': comment, 'image': reader.result});
+                socket.emit('send-message', {'comment': comment, 'filename': file.name , 'filetype': file.type, 'file': reader.result});
             }, false);
         }
         else{
