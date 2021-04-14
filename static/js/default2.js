1d07236e642b8e55192c3ae018daa7c8b90edc01
zhongens <54745798+zhongens@users.noreply.github.com>
2021-04-13 22:25:31 -0400

    Add files via upload
    combine online users + DM

--- static/js/default2.js
+++ static/js/default2.js
@@ -3,6 +3,18 @@ $(document).ready(function() {
 //    namespace = '/test';
 //    var socket = io(namespace);
     var socket = io()
+
+    socket.on('new_user', function(user) {
+        var users = document.getElementById('current_users');
+        var all_users = users.innerHTML;
+        if (!all_users.includes(user.user)){
+            var new_user = '<img src="static/images/' + user.icon + '" width=50/>';
+            new_user += '<a href="user_profile/' + user.username + '"> ' + user.username + '</a><br/>';
+            users.innerHTML += new_user;
+        }
+    });
+
+
     socket.on('blog_done', function(record) {
         var history = document.getElementById('chat');
         response = "<div class='history'><p>Uploaded by " + record.user + " by date: " + record.date + "</p><hr>";
@@ -17,7 +29,7 @@ $(document).ready(function() {
         var unread = document.getElementById('unread')
         unread.innerHTML = "UNREAD"
         alert("New message from "+msg.sender);
-        });
+    });
 
 
 
@@ -39,4 +51,29 @@ $(document).ready(function() {
         }
     });
 });
+
+getUsers();
+setInterval (getUsers, 5000);
+
+function getUsers() {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            renderUsers(this.response);
+        }
+    };
+    request.open("GET", "get-users");
+    request.send()
+}
+
+function renderUsers(rawUsers) {
+    let list = document.getElementById('current_users');
+    list.innerHTML = "";
+    var users = JSON.parse(rawUsers);
+    for (i = 0; i < users.length; i++){
+        var new_user = '<img src="static/images/' + users[i]['icon'] + '" width=50/>';
+        new_user += '<a href="user_profile/' + users[i]['username'] + '"> ' + users[i]['username'] + '</a><br/>';
+        list.innerHTML += new_user;
+    }
+}
 
